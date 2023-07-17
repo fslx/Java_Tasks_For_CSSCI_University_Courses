e994b44c2dbb85cbb903f9f85ea02b5aa9b9297c
fslx <jorgenmjos@hotmail.com>
2023-07-17 14:37:04 +0000

    changed gitignore

--- src/randomNumberGenerator.java
+++ src/randomNumberGenerator.java
@@ -4,7 +4,7 @@ public class randomNumberGenerator {
     public static void main(String[] args) {
         Random random = new Random();
 
-        int x = random.nextInt();
+        double x = random.nextDouble(24);
 
         System.out.println(x);
     }
