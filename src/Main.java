4ae4e179fb838368cf954ad265a81b9f017755f3
fslx <51758889+fslx@users.noreply.github.com>
2023-08-11 11:44:41 +0200

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,55 @@
+package Java;
 import java.lang.System;
-import java.lang.Math;
-import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        double x; // x val - 
-        double y; // y val 
-        double z; // hypotenus
-
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Enter side of x: ");
-        x = scanner.nextDouble();
-        System.out.println("Enter side of y: ");
-        y = scanner.nextDouble();
+    public static void main(String[] args) throws InterruptedException{
         
-        z = Math.sqrt((x*x)+(y*y));
+        class Counter {
+            private int count = 0;
+            public void increment() {++count;} // the statement that increases the value of one, will only be executed in ONE thread at most.
+            /*
+             * getfield count
+             * load 1 - both threads loads 1 each
+             * add - both threads adds 1
+             * putfield count
+             * Read-Modify-Write pattern
+             * RMW
+             */
+            public int getCount() {return count;}
+        }
 
-        System.out.println("The hypotenus is : " + z);
-    }
-}
+        final Counter counter = new Counter();
+
+        class CountingThread extends Thread {
+            public void run() {
+                for(int i = 0; i < 10000; i++) {
+                    counter.increment();
+                }
+            }
+        }
+
+        CountingThread t1 = new CountingThread();
+        CountingThread t2 = new CountingThread();
+
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+
+        System.out.println(counter.getCount()); // expected output: 20000
+
+        /*
+         * initalization of new threads below 
+         */
+
+    //     Thread myThread = new Thread() {
+    //     public void run() {
+    //         System.out.println("Hello from the new thread");
+    //     }
+    // };
+
+    // myThread.start();
+    // Thread.yield();
+    // System.out.println("Hello from the main thread");
+    // myThread.join();
+}}
